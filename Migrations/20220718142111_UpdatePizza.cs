f3081bb9add8eae20c853e661c108e2b38d8f176
Simone Mauro <simon.mauro@virgilio.it>
2022-07-18 16:25:44 +0200

    Validazioni aggiunte

---
+++ Migrations/20220718142111_UpdatePizza.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace la_mia_pizzeria_static.Migrations
+{
+    public partial class UpdatePizza : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pizzas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Immagine",
+                table: "Pizzas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pizzas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Immagine",
+                table: "Pizzas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_static.Models
@@ -5,11 +6,20 @@ namespace la_mia_pizzeria_static.Models
     public class Pizza{
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo seguente è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il campo seguente non deve superare i 50 caratteri")]
         public string Nome { get; set; }
 
         [Column(TypeName = "Text")]
-        public string Descrizione { get; set; }
-        public string Immagine { get; set; }
+        [StringLength(300, ErrorMessage = "Il campo seguente non deve superare i 300 caratteri")]
+
+
+        public string? Descrizione { get; set; }
+        public string? Immagine { get; set; }
+
+        [Required(ErrorMessage = "Il campo seguente è obbligatorio")]
+        [MinLength(1)]
         public double Prezzo { get; set; }
 
         public Pizza(string nome, string descrizione, string immagine, double prezzo){

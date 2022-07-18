4a58eda0eac0653683cf95961109f78123fed91c
Simone Mauro <simon.mauro@virgilio.it>
2022-07-18 14:15:21 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index(){
+            List<Pizza> pizze = new PizzaContext().Pizzas.ToList();
+            ViewData["title"] = "Menù pizze";
+            return View(pizze);
+        }
+
+        public IActionResult Show(int id){
+            Pizza pizzaResult = new PizzaContext().Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+            if (pizzaResult == null){
+                return NotFound($"Non esiste nessuna pizza con l'id {id}");
+            }
+            else{
+                return View(pizzaResult);
+
+            }
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza{
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        [Column(TypeName = "Text")]
+        public string Descrizione { get; set; }
+        public string Immagine { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza(string nome, string descrizione, string immagine, double prezzo){
+            Nome = nome;
+            Descrizione = descrizione;
+            Immagine = immagine;
+            Prezzo = prezzo;
+        }
+    }
+}
